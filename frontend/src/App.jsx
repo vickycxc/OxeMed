030cf5bb5f12c35d493a8b2c3ac40ec43ffb75fb
anabelaaulia <anabelasain274@gmail.com>
2025-05-31 12:26:57 +0700

    menambahkan login dokter

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import Dashboard from './Dashboard.jsx'
 import Riwayat from './Riwayat.jsx'
 import Konsultasi from './Konsultasi.jsx'
 import Login from './Login.jsx'
+import LoginDokter from './LoginDokter.jsx'
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
           <Route path="/test" element={<Test />} />
           <Route path="/riwayat" element={<Riwayat />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/logindokter" element={<LoginDokter />} />
         </Routes>
     </div>
   )

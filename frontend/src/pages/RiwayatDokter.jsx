c854c308a5b632140b9407433354877775fb2986
anabelaaulia <anabelasain274@gmail.com>
2025-06-12 11:55:39 +0700

    fix navigation 3

--- frontend/src/pages/RiwayatDokter.jsx
+++ frontend/src/pages/RiwayatDokter.jsx
@@ -26,12 +26,12 @@ const RiwayatDokter = () => {
 
   // Fungsi untuk mengarahkan ke halaman konsultasi
   const handleOngoingClick = () => {
-    navigate("/konsultasidokter");
+    navigate("/konsultasi");
   };
 
   // Fungsi untuk mengarahkan ke halaman RiwayatDokter.jsx
   const handleViewHistory = () => {
-    navigate("/riwayatdokter"); // Menavigasi ke halaman RiwayatDokter
+    navigate("/riwayat"); // Menavigasi ke halaman RiwayatDokter
   };
   const { logout } = useAuthStore();
 
